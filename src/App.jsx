3a9ae47c23ce089b91245f294918cce545d711c4
Yuliana Briukhovetska <yuliana111007@gmail.com>
2026-07-06 21:58:05 +0300

    resolved the issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,14 @@ import './App.scss';
 // Move each BEM block to a separate component (file) and import them here
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
-import Navigation from './components/Navigation/Navigation';
 import Welcome from './components/Welcome/Welcome';
-
 function App() {
   return (
     <main className="app">
+      <Welcome />
       <Header />
       <Article />
-      <Navigation />
-      <Welcome />
     </main>
   );
 }
-
 export default App;
